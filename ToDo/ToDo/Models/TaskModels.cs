ed697764bca4e9804857730e30b3b4f56ce41de5
Edwar Baron <edwar.baron@gmail.com>
2016-04-06 00:01:51 -0430

    Basic Task (Model, Controller, View)

---
+++ ToDo/ToDo/Models/TaskModels.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ToDo.Models
+{
+    public class TaskContext : DbContext
+    {
+        public DbSet<TaskModels> TaskModels { get; set; }
+    }
+
+    public class TaskModels
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public int UserId { get; set; }
+        [Required]
+        [Display(Name = "Titulo")]
+        public string Title { get; set; }
+        [Display(Name = "Descripcion")]
+        public string Description { get; set; }
+        [Display(Name = "¿Completada?")]
+        public bool IsComplete { get; set; }
+        [Display(Name = "Prioridad")]
+        public string Priority { get; set; }
+        [Display(Name = "Fecha de expiración")]
+        public DateTime? ExpirationDate { get; set; }
+        [Display(Name = "Fecha de creación")]
+        public DateTime? CreatedAt { get; set; }
+        [Display(Name = "Fecha de actualización")]
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
