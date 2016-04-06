6351eefa438329dd5cdc5288750e49489bd67ebf
Edwar Baron <edwar.baron@gmail.com>
2016-04-06 11:09:10 -0430

    Add template and Corrections

--- ToDo/ToDo/Controllers/HomeController.cs
+++ ToDo/ToDo/Controllers/HomeController.cs
@@ -3,15 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ToDo.Models;
 
 namespace ToDo.Controllers
 {
     public class HomeController : Controller
     {
+        private TaskContext dbTask = new TaskContext();
+        private UsersContext dbUser = new UsersContext();
+
         public ActionResult Index()
         {
             ViewBag.Message = "Modifique esta plantilla para poner en marcha su aplicación ASP.NET MVC.";
-
+            ViewBag.numTask = dbTask.TaskModels.ToList().Count();
+            ViewBag.numUser = dbUser.UserProfiles.ToList().Count();
             return View();
         }
 
@@ -22,11 +27,5 @@ namespace ToDo.Controllers
             return View();
         }
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Página de contacto.";
-
-            return View();
-        }
     }
 }
